31ea6b0ce3b52686c24a364bee6bacbc78f825e5
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:39:41 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

---
+++ src/main/java/com/jwebmp/plugins/angularzoomanimation/implementations/AngularZoomInExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.angularzoomanimation.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AngularZoomInExclusionsModule
+		implements IGuiceScanModuleExclusions<AngularZoomInExclusionsModule>,
+				           IGuiceScanJarExclusions<AngularZoomInExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-angular-ui-zoom-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.angularzoomanimation");
+		return strings;
+	}
+}
