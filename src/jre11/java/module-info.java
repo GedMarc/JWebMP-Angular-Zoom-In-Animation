6175ba0218848456262631a21ec03dc51d51e97d
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:13:43 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,11 +1,3 @@
-import com.jwebmp.core.base.angular.services.IAngularDirective;
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.angularzoomanimation.AngularZoomInAnimationPageConfigurator;
-import com.jwebmp.plugins.angularzoomanimation.AngularZoomInDirective;
-import com.jwebmp.plugins.angularzoomanimation.implementations.AngularZoomInExclusionsModule;
-
 module com.jwebmp.plugins.angularzoomanimation {
 	exports com.jwebmp.plugins.angularzoomanimation;
 
@@ -14,9 +6,9 @@ module com.jwebmp.plugins.angularzoomanimation {
 	requires com.jwebmp.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
-	provides IPageConfigurator with AngularZoomInAnimationPageConfigurator;
-	provides IAngularDirective with AngularZoomInDirective;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.angularzoomanimation.AngularZoomInAnimationPageConfigurator;
+	provides com.jwebmp.core.base.angular.services.IAngularDirective with com.jwebmp.plugins.angularzoomanimation.AngularZoomInDirective;
 
-	provides IGuiceScanJarExclusions with AngularZoomInExclusionsModule;
-	provides IGuiceScanModuleExclusions with AngularZoomInExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.angularzoomanimation.implementations.AngularZoomInExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.angularzoomanimation.implementations.AngularZoomInExclusionsModule;
 }
