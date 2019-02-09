f37069372274892d5c34562ba165e7a19c62fb5c
GedMarc <ged_marc@hotmail.com>
2019-02-09 11:52:07 +0200

    The Website API Brings out many things....

--- src/main/java/com/jwebmp/plugins/angularzoomanimation/AngularZoomInAnimationPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/angularzoomanimation/AngularZoomInAnimationPageConfigurator.java
@@ -82,7 +82,7 @@ public class AngularZoomInAnimationPageConfigurator
 	@Override
 	public Page configure(Page page)
 	{
-		if (!page.isConfigured())
+		if (!page.isConfigured() && enabled())
 		{
 			JQueryPageConfigurator.setRequired(true);
 			AngularPageConfigurator.setRequired(true);
