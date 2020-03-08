f698b451c6a31ba92c55a64d6b204baaa6fa2dd2
GedMarc <ged_marc@hotmial.com>
2020-03-08 16:59:31 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -9,6 +9,5 @@ module com.jwebmp.plugins.angularzoomanimation {
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.angularzoomanimation.AngularZoomInAnimationPageConfigurator;
 	provides com.jwebmp.core.base.angular.services.IAngularDirective with com.jwebmp.plugins.angularzoomanimation.AngularZoomInDirective;
 
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.angularzoomanimation.implementations.AngularZoomInExclusionsModule;
 	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.angularzoomanimation.implementations.AngularZoomInExclusionsModule;
 }
--- src/main/java/com/jwebmp/plugins/angularzoomanimation/implementations/AngularZoomInExclusionsModule.java
+++ src/main/java/com/jwebmp/plugins/angularzoomanimation/implementations/AngularZoomInExclusionsModule.java
@@ -1,6 +1,5 @@
 package com.jwebmp.plugins.angularzoomanimation.implementations;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,17 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class AngularZoomInExclusionsModule
-		implements IGuiceScanModuleExclusions<AngularZoomInExclusionsModule>,
-				           IGuiceScanJarExclusions<AngularZoomInExclusionsModule>
+		implements IGuiceScanModuleExclusions<AngularZoomInExclusionsModule>
 {
-
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("jwebmp-angular-ui-zoom-*");
-		return strings;
-	}
 
 	@Override
 	public @NotNull Set<String> excludeModules()
